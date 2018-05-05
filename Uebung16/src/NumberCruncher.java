270322a217bc449c8c88ce4766ae90bc651ba78f
Nico <Nico@192.168.1.100>
2018-05-05 15:59:40 +0200

    toString ergänzt

--- Uebung16/src/NumberCruncher.java
+++ Uebung16/src/NumberCruncher.java
@@ -97,5 +97,14 @@ public class NumberCruncher {
 	averageCount /= (laengeArray);
 	floatArray[indexOfBiggest] = averageCount;
     }
+    
+    @Override
+    public String toString() {
+	StringBuilder sb = new StringBuilder("");
+	for (int i=0; i < laengeArray; i++) {
+	    sb.append(i + ": " + floatArray[i] + "\n");
+	}
+	return sb.toString();
+    }
 
 }
