b3bdb6226c7eae68552e570de7a7cd41be815c0f
Nico <Nico@192.168.1.100>
2018-05-05 16:21:20 +0200

    Anonymous classes An

---
+++ Uebung16/src/NumberCruncherAnon.java
@@ -0,0 +1,119 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Beschreiben Sie hier die Klasse NumberCruncherAnon.
+ * 
+ * @author Nico Spanier, Matthias Tritt
+ * @version 05.05.18
+ */
+public class NumberCruncherAnon {
+    float[] floatArray;
+    Random rnd;
+    int laengeArray;
+
+    public NumberCruncherAnon(int[] args) {
+	laengeArray = args[0];
+	floatArray = new float[laengeArray];
+	for (int i = 0; i < laengeArray; i++) {
+	    rnd = new Random();
+	    floatArray[i] = rnd.nextFloat() + 1;
+	}
+
+    }
+
+    interface Operation {
+	void doOperation();
+    }
+
+    public void crunch(String[] operations) {
+	for (String operation : operations) {
+	    switch (operation) {
+	    case "sum":
+		new Operation() {
+		    @Override
+		    public void doOperation() {
+			for (int i = 1; i < floatArray.length; i++) {
+			    floatArray[i] += floatArray[i - 1];
+			}
+		    }
+		};
+		break;
+	    case "swirl":
+		new Operation() {
+		    @Override
+		    public void doOperation() {
+			for (int i = 0; i < laengeArray; i++) {
+			    int randomIndex1 = rnd.nextInt(laengeArray);
+			    int randomIndex2 = rnd.nextInt(laengeArray);
+
+			    float temp = floatArray[randomIndex1];
+			    floatArray[randomIndex1] = floatArray[randomIndex2];
+			    floatArray[randomIndex2] = temp;
+			}
+		    }
+		};
+		break;
+	    case "divide":
+		new Operation() {
+		    @Override
+		    public void doOperation() {
+			float[] sortedArray = floatArray.clone();
+			Arrays.sort(sortedArray);
+
+			for (int i = 0; i < laengeArray; i++) {
+			    floatArray[i] = sortedArray[laengeArray - i - 1] / sortedArray[i];
+			}
+		    }
+		};
+		break;
+	    case "subtract":
+		new Operation() {
+		    @Override
+		    public void doOperation() {
+			for (int i = 1; i < floatArray.length; i++) {
+			    floatArray[i] -= floatArray[i - 1];
+			}
+		    }
+		};
+		break;
+	    case "average":
+		new Operation() {
+		    @Override
+		    public void doOperation() {
+			float biggest = 0.0F;
+			float averageCount = 0.0F;
+			int indexOfBiggest = 0;
+
+			for (int i = 0; i < floatArray.length; i++) {
+			    if (biggest < floatArray[i]) {
+				biggest = floatArray[i];
+				indexOfBiggest = i;
+			    }
+			    averageCount += floatArray[i];
+			}
+			averageCount /= (laengeArray);
+			floatArray[indexOfBiggest] = averageCount;
+		    }
+		};
+		break;
+	    default:
+		System.out.println("Leere oder ungültige Eingabe");
+		break;
+	    }
+
+	}
+
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder sb = new StringBuilder("");
+	sb.append("Das floatArray mit einer Größe von " + laengeArray + ":" + "\n");
+	for (int i = 0; i < laengeArray; i++) {
+	    sb.append(i + ": " + floatArray[i] + "\n");
+	}
+	return sb.toString();
+    }
+
+}
